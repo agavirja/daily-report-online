58ed25cd6dad2e66b9df25765f79fc3865b134bf
agavirja <gaviriaalejandro@hotmail.com>
2023-05-23 11:28:51 -0500

    nuevo conector

--- datafunctions.py
+++ datafunctions.py
@@ -4,19 +4,21 @@ import numpy as np
 import json
 import mysql.connector as sql
 from shapely.geometry import  Polygon
+from sqlalchemy import create_engine 
 
 # Credenciales
 user     = st.secrets["user"]
 password = st.secrets["password"]
 host     = st.secrets["host"]
-schema = st.secrets["schema"]
+schema   = st.secrets["schema"]
+engine   = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/{schema}')
 
 @st.experimental_memo
 def propertydata():
-    db_connection       = sql.connect(user=user, password=password, host=host, database=schema)
-    data                = pd.read_sql("SELECT * FROM colombia.data_stock_inmuebles_gestion", con=db_connection) 
-    datacaracteristicas = pd.read_sql("SELECT * FROM colombia.data_stock_inmuebles_caracteristicas", con=db_connection)
-    dataimagenes        = pd.read_sql("SELECT * FROM colombia.data_stock_inmuebles_img", con=db_connection)
+    # db_connection       = sql.connect(user=user, password=password, host=host, database=schema)
+    data                = pd.read_sql_query("SELECT * FROM colombia.data_stock_inmuebles_gestion", engine) 
+    datacaracteristicas = pd.read_sql_query("SELECT * FROM colombia.data_stock_inmuebles_caracteristicas", engine)
+    dataimagenes        = pd.read_sql_query("SELECT * FROM colombia.data_stock_inmuebles_img", engine)
     
     vardrop = [x for x in list(data) if x in list(datacaracteristicas)]
     if any([x for x in vardrop if 'id_inmueble' in x]):
@@ -36,8 +38,8 @@ def propertydata():
 
 @st.experimental_memo
 def propertymanagementdata():
-    db_connection = sql.connect(user=user, password=password, host=host, database=schema)
-    data          = pd.read_sql("SELECT * FROM colombia.app_pm_cuentas", con=db_connection)
+    # db_connection = sql.connect(user=user, password=password, host=host, database=schema)
+    data          = pd.read_sql_query("SELECT * FROM colombia.app_pm_cuentas", engine)
     
     # Gasto del inmueble:
         # Restar del total de gasto (incluye los desembolsos) el valor de la compra
@@ -67,34 +69,34 @@ def propertymanagementdata():
 
 @st.experimental_memo
 def followup():
-    db_connection = sql.connect(user=user, password=password, host=host, database=schema)
-    data          = pd.read_sql("SELECT * FROM colombia.app_callcenter_callhistory_inbound", con=db_connection) 
+    # db_connection = sql.connect(user=user, password=password, host=host, database=schema)
+    data          = pd.read_sql_query("SELECT * FROM colombia.app_callcenter_callhistory_inbound", engine) 
     return data
 
 @st.experimental_memo
 def inspeccion_callsample():
-    db_connection = sql.connect(user=user, password=password, host=host, database=schema)
-    data          = pd.read_sql("SELECT * FROM colombia.app_callsample", con=db_connection) 
+    # db_connection = sql.connect(user=user, password=password, host=host, database=schema)
+    data          = pd.read_sql_query("SELECT * FROM colombia.app_callsample", engine) 
     data['fecha_asignacion_callagent']  = pd.to_datetime(data['fecha_asignacion_callagent'],errors='coerce')
     data['fecha_asignacion_visitagent'] = pd.to_datetime(data['fecha_asignacion_visitagent'],errors='coerce')
     return data
 
 @st.experimental_memo
 def inspeccion_visitas():
-    db_connection = sql.connect(user=user, password=password, host=host, database=schema)
-    data          = pd.read_sql("SELECT * FROM colombia.app_callsample", con=db_connection) 
+    # db_connection = sql.connect(user=user, password=password, host=host, database=schema)
+    data          = pd.read_sql_query("SELECT * FROM colombia.app_callsample", engine) 
     data['fecha_asignacion_callagent']  = pd.to_datetime(data['fecha_asignacion_callagent'],errors='coerce')
     data['fecha_asignacion_visitagent'] = pd.to_datetime(data['fecha_asignacion_visitagent'],errors='coerce')
 
     # Historial de llamadas 
-    datacallhistory  = pd.read_sql("SELECT * FROM colombia.app_callhistory;", con=db_connection) 
+    datacallhistory  = pd.read_sql_query("SELECT * FROM colombia.app_callhistory;", engine) 
 
     # Caracteristicas
-    datacaracteristicas = pd.read_sql("SELECT * FROM colombia.app_inspeccion_caracteristicas;", con=db_connection) 
+    datacaracteristicas = pd.read_sql_query("SELECT * FROM colombia.app_inspeccion_caracteristicas;", engine) 
     # Fotos
-    datafotos    = pd.read_sql("SELECT * FROM colombia.app_seguimiento_fotos_visitas", con=db_connection)
+    datafotos    = pd.read_sql_query("SELECT * FROM colombia.app_seguimiento_fotos_visitas", engine)
     # Estado del ambiente 
-    dataambiente = pd.read_sql("SELECT * FROM colombia.app_inspection_ambiente", con=db_connection) 
+    dataambiente = pd.read_sql_query("SELECT * FROM colombia.app_inspection_ambiente", engine) 
 
     grupofotos            = datafotos.groupby('id_unique')['url_foto'].apply('|'.join).reset_index()
     grupofotos.columns    = ['id','url_foto']
@@ -107,23 +109,23 @@ def inspeccion_visitas():
 
 @st.experimental_memo
 def inspeccion_callhistory():
-    db_connection = sql.connect(user=user, password=password, host=host, database=schema)
-    data          = pd.read_sql("SELECT * FROM colombia.app_callhistory", con=db_connection)
+    # db_connection = sql.connect(user=user, password=password, host=host, database=schema)
+    data          = pd.read_sql_query("SELECT * FROM colombia.app_callhistory", engine)
     data['call_date']  = pd.to_datetime(data['call_date'],errors='coerce')
     return data     
     
 @st.experimental_memo
 def inspeccion_seguimiento_ofertas():
-    db_connection = sql.connect(user=user, password=password, host=host, database=schema)
-    data          = pd.read_sql("SELECT * FROM colombia.data_stock_inmuebles_ofertas", con=db_connection)
+    # db_connection = sql.connect(user=user, password=password, host=host, database=schema)
+    data          = pd.read_sql_query("SELECT * FROM colombia.data_stock_inmuebles_ofertas", engine)
     return data  
 
 @st.experimental_memo
 def inspeccion_seguimiento_ofertas_merge():
-    db_connection = sql.connect(user=user, password=password, host=host, database=schema)
-    data          = pd.read_sql("SELECT * FROM colombia.data_stock_inmuebles_ofertas", con=db_connection)
+    # db_connection = sql.connect(user=user, password=password, host=host, database=schema)
+    data          = pd.read_sql_query("SELECT * FROM colombia.data_stock_inmuebles_ofertas", engine)
     idlist        = 'id_inmueble='+' OR id_inmueble='.join(data[data['id_inmueble'].notnull()]['id_inmueble'].astype(int).astype(str).unique())
-    datacaracteristicas = pd.read_sql(f"SELECT * FROM colombia.app_inspeccion_caracteristicas WHERE {idlist}", con=db_connection)
+    datacaracteristicas = pd.read_sql_query(f"SELECT * FROM colombia.app_inspeccion_caracteristicas WHERE {idlist}", engine)
     vardrop = [x for x in list(datacaracteristicas) if x in list(data)]
     vardrop.remove('id_inmueble')
     datacaracteristicas.drop(columns=vardrop,inplace=True)
@@ -132,14 +134,14 @@ def inspeccion_seguimiento_ofertas_merge():
 
 @st.experimental_memo
 def datadocumentos():
-    db_connection = sql.connect(user=user, password=password, host=host, database=schema)
-    data          = pd.read_sql("SELECT * FROM colombia.data_stock_inmuebles_documents", con=db_connection) 
+    # db_connection = sql.connect(user=user, password=password, host=host, database=schema)
+    data          = pd.read_sql_query("SELECT * FROM colombia.data_stock_inmuebles_documents", engine) 
     return data
 
 @st.experimental_memo
 def datarecorridos():
-    db_connection = sql.connect(user=user, password=password, host=host, database=schema)
-    data          = pd.read_sql("SELECT fecha_recorrido,nombre_conjunto,direccion_formato, direccion_porteria, upzcodigo,scanombre,tipo_negocio,telefono1, telefono2,telefono3, latitud,longitud,url_foto_fachada,url_foto_direccion,status,coddir FROM colombia.app_recorredor_stock_ventanas", con=db_connection) 
+    # db_connection = sql.connect(user=user, password=password, host=host, database=schema)
+    data          = pd.read_sql_query("SELECT fecha_recorrido,nombre_conjunto,direccion_formato, direccion_porteria, upzcodigo,scanombre,tipo_negocio,telefono1, telefono2,telefono3, latitud,longitud,url_foto_fachada,url_foto_direccion,status,coddir FROM colombia.app_recorredor_stock_ventanas", engine) 
     return data
 
 @st.experimental_memo
